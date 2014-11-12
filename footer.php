3bc1468261f2d3eec23c750ece76e110e703465c
Spamol <spamol@ya.ru>
2014-11-12 16:48:55 +0400

    Homework(stage2)
    Delete css, final edits

--- footer.php
+++ footer.php
@@ -1,12 +1,11 @@
-<footer>
-    <div class="wrapSmall">&copy; 2014. Это мой сайт, пожалуйста, не копируйте и не воруйте его=)</div>
-    <div class="gradient"></div>
-</footer>
-<div class="openMobileMenu">&nbsp;</div>
+		<footer>
+		    <div class="wrapSmall">&copy; 2014. Это мой сайт, пожалуйста, не копируйте и не воруйте его=)</div>
+		    <div class="gradient"></div>
+		</footer>
+		<div class="openMobileMenu">&nbsp;</div>
 
-<script src="js/vendor/jquery-1.11.1.min.js"></script>
-        <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-        <script>//window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
         <script src="js/plugins.js"></script>
         <script src="js/jquery.form.min.js"></script>
